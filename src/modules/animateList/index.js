43f2a1398a574c4d85027a9d3cc55b860848dcdf
jeffery <jiang1125712@163.com>
2019-05-23 14:15:44 +0800

    更新旋转动画

--- src/modules/animateList/index.js
+++ src/modules/animateList/index.js
@@ -9,15 +9,17 @@ class AnimateList extends Component {
 
   componentDidMount() {
     const outWidth = $(".out_circle").width()
+    // const centerImgWidth = $(".center-img1").width()
+    // if (centerImgWidth > 0) {
+    //   $(".center-img1").height(centerImgWidth * 382 / 689)
+    // }
     $(".out_circle").height(outWidth)
     // 获取半径
     const rWidth = outWidth / 2
-
     // 获得弧度
     const radian = 2 * Math.PI / 360 * 60
     // 长边
     const langWidth = Math.sin(radian) * rWidth
-    // 短边
 
     // icon的长度
     const iconWidth = $(".img_top").width()
@@ -42,18 +44,20 @@ class AnimateList extends Component {
     return <div className="animateList-container">
       <Header/>
       <Card title="3D旋转动画效果或者卫星围绕旋转效果" bordered={false} style={{ width: "80%", marginTop: 90, marginLeft: "10%", height: 500 }}>
-        <div className="out_circle">
-          <div className="nav_circle r1">
-            <img className="center-img1" src={require("Images/animateList/center1.png")}/>
+        <div className="star-animate">
+          <div className="out_circle">
+            <div className="nav_circle r1">
+              <img className="center-img1" src={require("Images/animateList/1/star.png")}/>
               <div className="img_top img">
-                <img src={require("Images/animateList/icon1.png")}/>
+                <img src={require("Images/animateList/1/star1.png")}/>
               </div>
               <div className="img_bottom img">
-                <img src={require("Images/animateList/icon2.png")}/>
+                <img src={require("Images/animateList/1/star2.png")}/>
               </div>
               <div className="img_bottom2 img">
-                <img src={require("Images/animateList/icon3.png")}/>
+                <img src={require("Images/animateList/1/star3.png")}/>
               </div>
+            </div>
           </div>
         </div>
       </Card>
